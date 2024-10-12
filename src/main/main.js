ebf75006a08bd95900e2af3f3a3f8eb5deaec788
KostsPlayer <azka.nuril070@gmail.com>
2024-10-12 19:09:12 +0700

    add effect smooth scroll

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,7 @@
+import SmoothScroll from "../helpers/smoothscroll.js";
 import Navbar from "../components/navbar/navbar.js";
 import Hero from "../pages/home/hero/hero.js";
 
+SmoothScroll();
 Navbar();
 Hero();
