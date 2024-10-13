35de66e42bf862e36fb03d09d4f027f052584bb1
mieramensatu <sasakihaise985@gmail.com>
2024-10-14 01:43:04 +0700

    fix: change wauthparam

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,8 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
 
-wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk3MTAwNDA/dGV4dD13aDR0NWF1dGgw";
+wauthparam.auth_ws="d3NzOi8vZ3cudWxiaS5hYy5pZC93cy93aGF0c2F1dGgvcHVibGlj";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgw";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/dashboard"
 deleteCookie(wauthparam.tokencookiename);
