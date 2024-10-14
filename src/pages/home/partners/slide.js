cab08ff42b2880846af497254ea85de9b856be0c
KostsPlayer <azka.nuril070@gmail.com>
2024-10-15 04:09:05 +0700

    mockoup home page done

--- src/pages/home/partners/slide.js
+++ src/pages/home/partners/slide.js
@@ -1,5 +1,5 @@
-export default function slide(){
-    const logoSlide = document.querySelector('.logo-slide');
-    const clone = logoSlide.cloneNode(true);
-    logoSlide.parentNode.appendChild(clone);
+export default function slide() {
+  const logoSlide = document.querySelector(".logo-slide");
+  const clone = logoSlide.cloneNode(true);
+  logoSlide.parentNode.appendChild(clone);
 }
