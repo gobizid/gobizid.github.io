149a745526c4edf16acb8c66655fdfa407048487
mieramensatu <sasakihaise985@gmail.com>
2024-10-14 19:09:54 +0700

    change: slide to main.js

--- src/main/main.js
+++ src/main/main.js
@@ -3,9 +3,11 @@ import navbar from "../components/navbar/navbar.js";
 import hero from "../pages/home/hero/hero.js";
 import categories from "../pages/home/categories/categories.js";
 import catalog from "../pages/home/catalog/catalog.js";
+import slide from "../pages/home/partners/slide.js";
 
 smoothScroll();
 navbar();
 hero();
 categories();
 catalog();
+slide();
