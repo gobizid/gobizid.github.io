3944f6fad0b1b4f4fca61e50407e5a32a495d3b5
KostsPlayer <azka.nuril070@gmail.com>
2024-10-14 21:34:25 +0700

    fix bug on home page

--- src/main/main.js
+++ src/main/main.js
@@ -2,13 +2,12 @@ import smoothScroll from "../helpers/smoothscroll.js";
 import navbar from "../components/navbar/navbar.js";
 import hero from "../pages/home/hero/hero.js";
 import categories from "../pages/home/categories/categories.js";
-import catalog from "../pages/home/catalog/catalog.js";
+import product from "../pages/home/product/product.js";
 import slide from "../pages/home/partners/slide.js";
 
 smoothScroll();
 navbar();
 hero();
 categories();
-catalog();
+product();
 slide();
-
