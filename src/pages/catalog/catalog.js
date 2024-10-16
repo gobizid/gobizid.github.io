3d324166813dd3a2892eefe64f5838cb36acb3aa
KostsPlayer <azka.nuril070@gmail.com>
2024-10-16 18:18:00 +0700

    add a category filter for the catalog and its opening-closing toggle

---
+++ src/pages/catalog/catalog.js
@@ -0,0 +1,7 @@
+import smoothScroll from "../../helpers/smoothscroll.js";
+import navbar from "../../components/navbar/navbar.js";
+import filter from "./filter.js";
+
+smoothScroll();
+navbar();
+filter();
---
+++ src/pages/catalog/filter.js
@@ -0,0 +1,20 @@
+export default function filter() {
+  const filter = document.querySelector(".catalog .catalog-filter");
+  const controlFilter = document.querySelector(
+    ".catalog-display .controll-filter"
+  );
+
+  document
+    .querySelector(".filter-header .filter-header-close")
+    .addEventListener("click", () => {
+      controlFilter.classList.toggle("expanded");
+      filter.classList.toggle("hidden");
+    });
+
+  document
+    .querySelector(".catalog-display .controll-filter")
+    .addEventListener("click", () => {
+      controlFilter.classList.toggle("expanded");
+      filter.classList.toggle("hidden");
+    });
+}
