39c50dcc6034ae28b8bf2fcc05b5295a4e27abb1
KostsPlayer <azka.nuril070@gmail.com>
2024-10-20 22:13:50 +0700

    adding login page and its supporting functions

---
+++ src/pages/login/login.js
@@ -0,0 +1,9 @@
+import smoothScroll from "../../helpers/smoothscroll.js";
+import fetchLog from "./fetchlog.js";
+
+export default function login() {
+  smoothScroll();
+  fetchLog();
+}
+
+login();
