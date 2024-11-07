691a3eeba293a81d8d53eef644c1f08a052e92c6
KostsPlayer <azka.nuril070@gmail.com>
2024-11-07 23:59:40 +0700

    update some style

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,20 +1,30 @@
-export default function sidebarToggle() {
+export default function sidebarTogle() {
   const menuItems = document.querySelectorAll("aside.navigation .menu-item");
-  
   if (menuItems) {
     menuItems.forEach((menu) => {
       menu.addEventListener("click", function (e) {
         const menuName = this.getAttribute("data-menu");
 
-        if (menuName === "management-product") {
-          e.preventDefault();
-          window.location.href = "/table-product";
-
-          sessionStorage.setItem("activeSubmenu", "management-product-submenu");
-        } else {
-          console.log(`Navigasi ke ${menuName} tidak memiliki pengaturan khusus.`);
-        }
+        // Simpan menu yang aktif di localStorage
+        localStorage.setItem("activeMenu", menuName);
       });
     });
+  }
+
+  // Ketika halaman di-load, ambil nilai activeMenu dari localStorage dan tampilkan submenu yang sesuai
+  const activeMenu = localStorage.getItem("activeMenu");
+  if (activeMenu) {
+    // Sembunyikan semua submenu terlebih dahulu
+    document
+      .querySelectorAll(".app-header .app-header-navigation .submenu")
+      .forEach((submenu) => {
+        submenu.style.display = "none";
+      });
+
+    // Tampilkan submenu yang aktif
+    const activeSubmenu = document.querySelector(`.${activeMenu}-submenu`);
+    if (activeSubmenu) {
+      activeSubmenu.style.display = "flex";
+    }
   }
 }
--- src/pages/dashboard/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard/dashboard.js
@@ -2,17 +2,16 @@ import loadComponent from "/src/helpers/loadComponent.js";
 import sidebarTogle from "/src/components/sidebar/sidebar.js";
 
 export default function loadDashboard() {
-    const promises = [
-      loadComponent("aside.sidebar", "/src/components/sidebar/sidebar.html"),
-      loadComponent("header.topbar", "/src/components/topbar/topbar.html"),
-      loadComponent("section.user", "/src/pages/dashboard/product/product.html"),
-    ];
-  
-    Promise.all(promises)
+  const promises = [
+    loadComponent("aside.sidebar", "/src/components/sidebar/sidebar.html"),
+    loadComponent("header.topbar", "/src/components/topbar/topbar.html"),
+  ];
+
+  Promise.all(promises)
     .then(() => {
-      sidebarTogle()
+      sidebarTogle();
     })
-      .catch((error) => {
-        console.error("Error loading components:", error);
-      });
-  }
+    .catch((error) => {
+      console.error("Error loading components:", error);
+    });
+}
