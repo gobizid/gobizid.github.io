125212c43baead1bc1c3d477d06769bd47935af1
KostsPlayer <azka.nuril070@gmail.com>
2024-11-08 11:30:14 +0700

    fecth get categories

--- src/pages/management-product/categories/categories.js
+++ src/pages/management-product/categories/categories.js
@@ -1,29 +1,7 @@
 import loadComponent from "/src/helpers/loadComponent.js";
 import sidebarTogle from "/src/components/sidebar/sidebar.js";
 import smoothScroll from "/src/helpers/smoothScroll.js";
-
-function toggleOptions() {
-  const items = document.querySelectorAll(".content-main .item");
-
-  items.forEach((item) => {
-    const toggleButton = item.querySelector(".material-symbols-outlined");
-    const toggleContainer = item.querySelector(".toggle");
-
-    toggleButton.addEventListener("click", (e) => {
-      toggleContainer.classList.toggle("active");
-      e.stopPropagation();
-    });
-
-    document.addEventListener("click", (e) => {
-      if (
-        !toggleContainer.contains(e.target) &&
-        !toggleButton.contains(e.target)
-      ) {
-        toggleContainer.classList.remove("active");
-      }
-    });
-  });
-}
+import getAllCateogories from "/src/pages/management-product/categories/fetch.js";
 
 export default function loadManagementProductCategories() {
   const promises = [
@@ -39,7 +17,7 @@ export default function loadManagementProductCategories() {
     .then(() => {
       sidebarTogle();
       smoothScroll();
-      toggleOptions();
+      getAllCateogories();
     })
     .catch((error) => {
       console.error("Error loading components:", error);
